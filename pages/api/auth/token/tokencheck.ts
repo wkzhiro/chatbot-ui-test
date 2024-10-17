5006432405fddeac8052e49d513a29ecc63054c6
wkzhiro <wkzhiro0116@yahoo.co.jp>
2024-10-18 02:22:29 +0900

    stagingへ

--- pages/api/auth/token/tokencheck.ts
+++ pages/api/auth/token/tokencheck.ts
@@ -18,6 +18,7 @@ export  const refreshJWTbytoken = async (oid: string) => {
   const { data }: { data: AuthenticationResult } = await axios.put(url, {
     oid:oid
   });
+  console.log("refresh_oid_received", oid)
   const newToken = data.accessToken;
   return newToken;
 };
